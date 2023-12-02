64a4012a64361c7e5c2a86f8d0739db19d6636b6
Mohid123 <muhammadmohid141@gmail.com>
2023-12-02 18:38:07 +0500

    small fix to description

--- app/page.js
+++ app/page.js
@@ -3,11 +3,11 @@ import LightBulb from "@/components/light-bulb/light-bulb";
 export default function Home() {
   return (
     <main className="flex flex-col items-start justify-between w-full min-h-screen mt-20">
-      <div className="flex justify-between w-full bg-img">
-        <div className="flex flex-col">
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col w-1/2 mt-20">
           <p className="mb-3 text-5xl font-bold">Hi, I'm Mohid.</p>
           <p className="mb-4 text-5xl font-bold">Full Stack Web Developer.</p>
-          <p className="text-xl font-semibold">I have been working as a web developer for 4 years. I'm based in Pakistan.</p>
+          <p className="text-xl font-semibold">Crafting digital dreams for 4 years, I've been weaving the web for clients across the globe. As a seasoned web developer, I don't just code; I sculpt online experiences that leave a lasting impression. Let's turn your ideas into digital masterpieces together!</p>
           <button className='rounded-lg bg-[#ee7600] font-semibold p-4 w-[25%] mt-5 custom-shadow'>Hire Me!</button>
         </div>
         <LightBulb></LightBulb>
