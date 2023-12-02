a6bdb563008c48a9e0e0104dbbcf3709d4914d88
Mohid123 <muhammadmohid141@gmail.com>
2023-12-02 21:43:41 +0500

    about me added

--- components/specialty-card/specialty-card.js
+++ components/specialty-card/specialty-card.js
@@ -28,7 +28,7 @@ const SpecialtyCard = () => {
                 "NgRx"
             ];
             const options = {
-                radius: 300 ,
+                radius: 350 ,
                 maxSpeed: 'normal',
                 initSpeed: 'normal',
                 keep: true,
@@ -40,9 +40,9 @@ const SpecialtyCard = () => {
         <div className='flex justify-between'>
             <div className='w-1/2'>
                 <p className='text-5xl font-semibold'>About me</p>
-                <p className='mt-4 text-lg font-semibold'>About me About me About me About me About me About me About me About me About me About me About me About me About me About me About me About me About me </p>
+                <p className='mt-4 text-lg font-semibold'>I'm an experienced full-stack web developer with a unique background in space sciences, having earned my bachelor's degree in 2020. Over the past 4 years, I've honed my skills to specialize in crafting visually impactful frontends within the dynamic realm of web development. Away from the screen, I'm an avid reader, delving into the worlds of literature and anime for inspiration. On the field, my passion for strategy and teamwork shines through in my love for cricket and football. Coding is more than a profession for me; it's a compelling journey of exploration and innovation. Let's collaborate and bring your digital aspirations to fruition! </p>
             </div>
-            <div className="relative flex items-center justify-end w-full bg-inherit">
+            <div className="relative flex justify-end w-full bg-inherit">
                 <span className='tagcloud'></span>
             </div>
         </div>
