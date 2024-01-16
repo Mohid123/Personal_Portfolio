99ffaea3f144a558a630adc1ee95f1acd9adbf9d
Mohid123 <muhammadmohid141@gmail.com>
2024-01-16 18:30:43 +0500

    added resume to portfolio

--- app/page.js
+++ app/page.js
@@ -7,6 +7,8 @@ import Intro from "@/components/intro/intro";
 import LightBulb from "@/components/light-bulb/light-bulb";
 import MouseScroll from "@/components/mouse-scroll/mouse-scroll";
 import Portfolio from "@/components/portfolio/portfolio";
+import adobe from '../public/adobe.svg';
+import Image from 'next/image';
 const SpecialtyCard = dynamic(() => import('../components/specialty-card/specialty-card'), {
   loading: () => <p>Loading...</p>,
   ssr: false,
@@ -14,7 +16,10 @@ const SpecialtyCard = dynamic(() => import('../components/specialty-card/special
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full min-h-screen mt-20">
+    <main className="relative flex flex-col w-full min-h-screen mt-20">
+      <a href='/Mohid.pdf' download className='absolute right-0 p-1 bg-white rounded-md -top-14' title='Click to download Resume'>
+        <Image src={adobe} alt="adobe_acrobat"/>
+      </a>
       <div className="flex flex-col w-full lg:flex-row lg:justify-between">
         <Intro></Intro>
         <LightBulb></LightBulb>
